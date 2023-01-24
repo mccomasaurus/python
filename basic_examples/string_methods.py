c1caea155c3ff3d2f9485282b704e7919b2946b2
Matthew McComas <mccomasmatt@gmail.com>
2023-01-24 14:38:04 -0500

    rm basic_examples

--- basic_examples/string_methods.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-url = "www.redhat.com"
-result = url.startswith("www")
-print(result)
-print(url.endswith(".org"))
-new_url = url.upper()
-print("ORIGINAL", url, "RETURNED", new_url)
