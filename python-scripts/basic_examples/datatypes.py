eb39e515c6e442a3b020a6f4ecb7da8af36f3c54
mccomasaurus <37151414+mccomasaurus@users.noreply.github.com>
2023-02-01 14:57:37 -0500

    Update datatypes.py

--- python-scripts/basic_examples/datatypes.py
+++ python-scripts/basic_examples/datatypes.py
@@ -1,3 +1,9 @@
+# The value of what is being referenced by the variable has a type like int or float or str, but the variable itself has no type.
+# The built-in type() function returns the data type of the object that is referenced by a particular variable.
+# Notice that the type of the reference is bound dynamically as the program is executed.
+# The built-in id() function returns a unique identifier of an object that is referenced by a particular variable.
+# There is only a single int object 10 being created and both the variables x and y reference the same object. This is evident in that they both produce the same id.
+
 #!/usr/bin/env python3
 x = 10
 tab = " \t"
