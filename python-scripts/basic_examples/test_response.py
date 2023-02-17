abe326343a3bee555f6ab02088275c4223b4f63c
Matthew McComas <mccomasmatt@gmail.com>
2023-02-17 13:28:05 -0500

    test

---
+++ python-scripts/basic_examples/test_response.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+data = input("Enter some text: ")
+
+print("Ends with a period?", data.endswith("."))
+print("Is all alpha?", data.isalpha())
+print("Is 'x' in the text?", 'x' in data)
+print()    
+
